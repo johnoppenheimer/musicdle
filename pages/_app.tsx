445544c1c5d07fcc90419bcfbf5e74ef5f54d510
Maxime Cattet <maximecattet@gmx.com>
2022-05-05 21:48:55 +0200

    feat: Add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps}/>;
+const queryClient = new QueryClient();
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <QueryClientProvider client={queryClient}>
+        <Component {...pageProps}/>
+        {process.env.NODE_ENV !== 'production' && (
+            <ReactQueryDevtools />
+        )}
+    </QueryClientProvider>
+);
 
 export default MyApp;
