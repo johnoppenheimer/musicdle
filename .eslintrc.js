ce842b7098f2fb3c97bbfc3c94328e00ffcdb1c4
Maxime Cattet <maximecattet@gmx.com>
2022-05-05 19:43:59 +0200

    chore: Move back to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'next/core-web-vitals',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'warn',
+        'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
